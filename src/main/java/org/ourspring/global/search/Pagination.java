5030bc133cef447b3d89ad79eb63e8c0269f8f2d
juyonghyeon <jyh928578@gmail.com>
2025-06-10 16:41:55 +0900

    06/10 중간 푸시

--- src/main/java/org/ourspring/global/search/Pagination.java
+++ src/main/java/org/ourspring/global/search/Pagination.java
@@ -11,31 +11,33 @@ public class Pagination {
     private int total;
     private int range;
     private int limit;
+    private int firstRangePage;
+    private int lastRangePage;
+    private int prevRangePage;
+    private int nextRangePage;
+    private int lastPage;
 
     /**
      *
-     * @param page : 페이지 번호, 1,2,3,4
+     * @param page : 페이지 번호, 1, 2, 3, 4,
      * @param total : 총 레코드 갯수
      * @param range : 페이지 구간
      * @param limit : 한 페이지당 보여줄 레코드 갯수
      */
-    public Pagination(int page,int total, int range, int limit){
+    public Pagination(int page, int total, int range, int limit) {
         page = Math.max(page, 1);
         range = range < 1 ? 10 : range;
         limit = limit < 1 ? 20 : limit;
 
-        if (total<1) return;
+        if (total < 1) return;
 
         // 전체 페이지 갯수
         int totalPages = (int)Math.ceil(total / (double)limit);
 
         // 페이지 구간 번호 - 0, 1, 2 ...
         int rangeNum = (page - 1) / range;
-
         int firstRangePage = rangeNum * range + 1; // 현재 구간에서의 첫번째 페이지 번호
-
         int lastRangePage = firstRangePage + range - 1; // 현재 구간에서의 마지막 페이지 번호
-
         lastRangePage = Math.min(totalPages, lastRangePage); // 마지막 페이지는 총 페이지의 갯수 이하
 
         // 이전 구간의 마지막 페이지
@@ -43,7 +45,19 @@ public class Pagination {
         int prevRangePage = rangeNum > 0 ? firstRangePage - 1 : 0;
 
         // 다음 구간의 첫번째 페이지, 0 이면 다음 구간이 없다.
-        int lastRangeNum = (totalPages - 1) / range; // 파지막 페이지 구간 번호
+        int lastRangeNum = (totalPages - 1) / range; // 마지막 페이지 구간 번호
         int nextRangePage = rangeNum < lastRangeNum ? lastRangePage + 1 : 0;
+
+        this.page = page;
+        this.range = range;
+        this.limit = limit;
+        this.total = total;
+        this.lastPage = totalPages;
+        this.firstRangePage = firstRangePage;
+        this.lastRangePage = lastRangePage;
+        this.prevRangePage = prevRangePage;
+        this.nextRangePage = nextRangePage;
     }
+
+
 }
