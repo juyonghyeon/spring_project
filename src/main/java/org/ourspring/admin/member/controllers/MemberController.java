bd93933cb2b48f5d58684fc62cf38fd873112d28
juyonghyeon <jyh928578@gmail.com>
2025-06-10 17:29:58 +0900

    06/10_17:29

--- src/main/java/org/ourspring/admin/member/controllers/MemberController.java
+++ src/main/java/org/ourspring/admin/member/controllers/MemberController.java
@@ -1,15 +1,24 @@
 package org.ourspring.admin.member.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.ourspring.admin.global.controllers.CommonController;
+import org.ourspring.global.search.ListData;
+import org.ourspring.member.controllers.MemberSearch;
+import org.ourspring.member.entities.Member;
+import org.ourspring.member.services.MemberInfoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @RequestMapping("/admin/member")
 @Controller("adminMemberController")
 public class MemberController extends CommonController {
+
+    private final MemberInfoService infoService;
 
     @Override
     public String mainCode() {
@@ -17,8 +26,12 @@ public class MemberController extends CommonController {
     }
 
     @GetMapping({"", "/list"})
-    public String list(Model model) {
+    public String list(@ModelAttribute MemberSearch search, Model model) {
         commonProcess("list", model);
+
+        ListData<Member> data = infoService.getList(search);
+        model.addAttribute("items", data.getItems());
+        model.addAttribute("pagination", data.getPagination());
 
         return "admin/member/list";
     }
--- src/main/java/org/ourspring/member/services/MemberInfoService.java
+++ src/main/java/org/ourspring/member/services/MemberInfoService.java
@@ -1,7 +1,9 @@
 package org.ourspring.member.services;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.ourspring.global.search.ListData;
+import org.ourspring.global.search.Pagination;
 import org.ourspring.member.MemberInfo;
 import org.ourspring.member.constants.Authority;
 import org.ourspring.member.controllers.MemberSearch;
@@ -32,6 +34,8 @@ public class MemberInfoService implements UserDetailsService {
 
     private final MemberRepository repository;
     private final JdbcTemplate jdbcTemplate;
+
+    private final HttpServletRequest request; // 쿼리스트링 값 유지를 위해
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -102,23 +106,32 @@ public class MemberInfoService implements UserDetailsService {
         }
         // 권한 조건 검색 E
 
-        params.add(offset);
-        params.add(limit);
 
         StringBuffer sb = new StringBuffer(2000);
+        StringBuffer sb2 = new StringBuffer(2000);
+
         sb.append("SELECT * FROM MEMBER");
+        sb2.append("SELECT COUNT(*) FROM MEMBER");
 
         if (!addWhere.isEmpty()) {
-            sb.append(" WHERE ");
-            sb.append(String.join(" AND ", addWhere));
+            String where = " WHERE " + String.join(" AND ", addWhere);
+            sb.append(where);
+            sb.append(where);
         }
 
         sb.append(" ORDER BY createdAt DESC");
         sb.append(" LIMIT ?, ?");
 
+        params.add(offset);
+        params.add(limit);
+
         List<Member> items = jdbcTemplate.query(sb.toString(), this::mapper, params.toArray());
 
-        return null;
+        int total = jdbcTemplate.queryForObject(sb2.toString(), int.class); // 검색 조건에 다른 전체 레코드 갯수
+
+        Pagination pagination = new Pagination(page, total, 10, 20, request);
+
+        return new ListData<>(items,pagination);
     }
 
     private Member mapper(ResultSet rs, int i) throws SQLException {
