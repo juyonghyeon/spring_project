ec79c726c7ca8c7e64d7e1afc2dd8d88dd9d9213
juyonghyeon <jyh928578@gmail.com>
2025-06-11 20:10:04 +0900

    06/11 저장

--- src/main/java/org/ourspring/global/libs/Utils.java
+++ src/main/java/org/ourspring/global/libs/Utils.java
@@ -36,27 +36,32 @@ public class Utils {
 
     /**
      * 휴대폰 장비 인지, PC 인지
+     *
      * @return
      */
     public boolean isMobile() {
         String ua = request.getHeader("User-Agent");
+
         String pattern = ".*(iPhone|iPod|iPad|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson).*";
-      
+
         return StringUtils.hasText(ua) && ua.matches(pattern);
     }
 
     /**
      * mobile, front 템플릿을 분리
+     *
      * @param path
      * @return
      */
     public String tpl(String path) {
         String prefix = isMobile() ? "mobile" : "front";
+
         return String.format("%s/%s", prefix, path);
     }
 
     /**
      * 메세지를 코드로 조회
+     *
      * @param code
      * @return
      */
@@ -64,5 +69,9 @@ public class Utils {
         Locale locale = localeResolver.resolveLocale(request);
 
         return messageSource.getMessage(code, null, locale);
+    }
+
+    public String getParam(String name) {
+        return request.getParameter(name);
     }
 }
