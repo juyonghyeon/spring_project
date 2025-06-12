879c85cf39a37d0fa4839037f414770da0ba8223
juyonghyeon <jyh928578@gmail.com>
2025-06-12 23:24:45 +0900

    ProductManageService 준비 작업

--- src/main/java/org/ourspring/product/services/ProductManageService.java
+++ src/main/java/org/ourspring/product/services/ProductManageService.java
@@ -4,25 +4,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.ourspring.global.exceptions.script.AlertException;
 import org.ourspring.global.libs.Utils;
+import org.ourspring.product.entities.Product;
+import org.ourspring.product.repositories.ProductRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Lazy
 @Service
 @RequiredArgsConstructor
 public class ProductManageService {
 
+    private final HttpServletRequest request;
+    private final ProductRepository repository;
+
     public void processBatch(List<Integer> chks) {
 
         if (chks == null || chks.isEmpty()) {
             throw new AlertException("처리할 상품을 선택하세요.", HttpStatus.BAD_REQUEST);
         }
-
-
-
 
     }
 
