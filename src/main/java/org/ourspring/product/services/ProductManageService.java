8c6e85866dab410a3018d1846be9d3cfeefb64b5
KimMarinet <kiana507680@gmail.com>
2025-06-13 00:16:50 +0900

    [list]검수 완

--- src/main/java/org/ourspring/product/services/ProductManageService.java
+++ src/main/java/org/ourspring/product/services/ProductManageService.java
@@ -1,9 +1,7 @@
 package org.ourspring.product.services;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.ourspring.global.exceptions.script.AlertException;
-import org.ourspring.global.libs.Utils;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -20,8 +18,6 @@ public class ProductManageService {
         if (chks == null || chks.isEmpty()) {
             throw new AlertException("처리할 상품을 선택하세요.", HttpStatus.BAD_REQUEST);
         }
-
-
 
 
     }
