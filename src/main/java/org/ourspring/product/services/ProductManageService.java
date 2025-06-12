efd9d9b5aa458d1e9351e74375dde00aba78765a
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-06-12 23:22:32 +0900

    회원 탈퇴 기능 만들 었습니다

--- src/main/java/org/ourspring/product/services/ProductManageService.java
+++ src/main/java/org/ourspring/product/services/ProductManageService.java
@@ -4,10 +4,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.ourspring.global.exceptions.script.AlertException;
 import org.ourspring.global.libs.Utils;
+import org.ourspring.member.entities.Member;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Lazy
@@ -15,11 +17,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductManageService {
 
+    private final HttpServletRequest request;
+    private final Member member;
     public void processBatch(List<Integer> chks) {
-
+        String method = request.getMethod();
         if (chks == null || chks.isEmpty()) {
             throw new AlertException("처리할 상품을 선택하세요.", HttpStatus.BAD_REQUEST);
         }
+
+        if(method.equalsIgnoreCase("DELETE")){ // 탈퇴 회원 기능
+         member.setDeletedAt(LocalDateTime.now());
+        }
+
 
 
 
